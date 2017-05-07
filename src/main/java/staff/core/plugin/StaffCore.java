159e3352e4603018ddced1992bfbaa4df3208403
Aerh <callum.aero@gmail.com>
2017-05-07 15:24:58 +0100

    professionalism doesn't want weird messages

--- src/main/java/staff/core/plugin/StaffCore.java
+++ src/main/java/staff/core/plugin/StaffCore.java
@@ -11,7 +11,7 @@ public class StaffCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&cCTF StaffCore &7Enabling..."));
+
         getCommand("s").setExecutor(new StaffChat());
         getCommand("ban").setExecutor(new BanCommand(this));
         getCommand("kick").setExecutor(new KickCommand(this));
@@ -19,10 +19,5 @@ public class StaffCore extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new FilterListener(), this);
         getServer().getPluginManager().registerEvents(new GMCommand(), this);
-    }
-
-    @Override
-    public void onDisable() {
-        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&cCTF StaffCore &7Reloading..."));
     }
 }
