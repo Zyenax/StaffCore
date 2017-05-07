d2495e8286d9ed83be0c035ab29003dd00c21c13
Draem <owen@gimbel.ca>
2017-05-07 10:48:47 -0400

    Added IP Command and removed some imports.

--- src/main/java/staff/core/plugin/StaffCore.java
+++ src/main/java/staff/core/plugin/StaffCore.java
@@ -1,11 +1,10 @@
 package staff.core.plugin;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import staff.core.plugin.commands.GMCommand;
 import staff.core.plugin.commands.StaffChat;
 import staff.core.plugin.commands.punishments.BanCommand;
+import staff.core.plugin.commands.punishments.IPCommand;
 import staff.core.plugin.commands.punishments.KickCommand;
 import staff.core.plugin.commands.punishments.WarnCommand;
 import staff.core.plugin.listeners.FilterListener;
@@ -14,12 +13,12 @@ public class StaffCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
         getCommand("s").setExecutor(new StaffChat());
         getCommand("ban").setExecutor(new BanCommand());
         getCommand("kick").setExecutor(new KickCommand());
         getCommand("gmute").setExecutor(new GMCommand());
         getCommand("warn").setExecutor(new WarnCommand());
+        getCommand("ip").setExecutor(new IPCommand());
 
         getServer().getPluginManager().registerEvents(new FilterListener(), this);
         getServer().getPluginManager().registerEvents(new GMCommand(), this);
---
+++ src/main/java/staff/core/plugin/commands/punishments/IPCommand.java
@@ -0,0 +1,36 @@
+package staff.core.plugin.commands.punishments;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by Draem on 5/7/2017.
+ */
+public class IPCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
+        if (!sender.hasPermission("warvale.admin")) {
+            sender.sendMessage(ChatColor.RED + "You don't have permission to access this command.");
+            return true;
+        }
+
+        if (args.length != 2) {
+            sender.sendMessage(args.length > 2 ? ChatColor.RED + "Not enough arguments." : ChatColor.RED + "Too many arguments.");
+            return true;
+        }
+
+        Player player = Bukkit.getPlayer(args[1]);
+        if (!(player == null)) {
+            sender.sendMessage(ChatColor.GREEN + player.getName() + "'s IP: " + ChatColor.WHITE + player.getAddress().getAddress().getHostAddress() + " : " + player.getAddress().getPort());
+        } else {
+            sender.sendMessage(ChatColor.RED + "Invalid player!");
+        }
+        return true;
+    }
+
+}
