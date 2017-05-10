ed2a887c511e44e0d07ba372ea1447ca8b5a3fca
Draem <owen@gimbel.ca>
2017-05-10 16:09:06 -0400

    rank constructor

--- src/main/java/me/warvale/staffcore/rank/Rank.java
+++ src/main/java/me/warvale/staffcore/rank/Rank.java
@@ -1,7 +1,15 @@
 package me.warvale.staffcore.rank;
 
+import java.security.Permission;
+import java.util.List;
+
 /**
  * Created by Draem on 5/10/2017.
  */
 public class Rank {
+
+    public Rank(String name, String prefix, boolean staff, List<Rank> parents, List<Permission> permissions) {
+
+    }
+
 }
