6560067a3a24e4589dd2fea965dc2c4e58836ac3
Eric <ehock7229@outlook.com>
2017-05-22 18:03:21 -0400

    fix chat formatting

--- src/main/java/net/warvale/staffcore/listeners/ChatListener.java
+++ src/main/java/net/warvale/staffcore/listeners/ChatListener.java
@@ -31,7 +31,7 @@ public class ChatListener implements Listener {
 
         User user = UserManager.getUser(event.getPlayer());
 
-        event.setFormat(ChatColor.translateAlternateColorCodes('&', user.getPrefix()) + ChatColor.RESET + " " +
+        event.setFormat(ChatColor.translateAlternateColorCodes('&', user.getPrefix()) + ChatColor.RESET +
                 ChatColor.BLUE + user.getName() + ChatColor.RESET + ": " + event.getMessage());
     }
 
